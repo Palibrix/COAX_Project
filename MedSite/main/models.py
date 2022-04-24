dda2bb3104a8d398cbef00f37a326b18be34bb04
palibrix <theopaolo04@gmail.com>
2022-04-24 22:06:33 +0300

    Added .env (everyone should create that file for themselves) use "pip install django-phonenumber-field[phonenumbers]" to install phonenumber library Also added first version of DB about user, check it out

--- MedSite/main/models.py
+++ MedSite/main/models.py
@@ -1,3 +1,21 @@
+import uuid
+from phonenumber_field.modelfields import PhoneNumberField
 from django.db import models
 
+
+class user(models.Model):
+    name = models.CharField(max_length=255, null=False, blank=False, help_text="Enter your name")
+    surname = models.CharField(max_length=255, null=False, blank=False, help_text="Enter your surname")
+    dateOfBirth = models.DateField(help_text="Pick your birth date")
+    phone_number = PhoneNumberField(null=False, blank=False, unique=True)
+    email = models.EmailField(unique=True)
+    # language = models.Choices[
+    #     ('UA', 'Українська'),
+    #     ('EN', 'English'),
+    # ]
+    inv_code = models.CharField(max_length=255, null=False, blank=False, help_text="Your invitation code")
+    uid = uuid.uuid4()
+
+    class Meta:
+        db_table = 'users'
 # Create your models here.
