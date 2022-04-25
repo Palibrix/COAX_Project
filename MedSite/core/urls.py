202fc0000109e31388ed017190b2e95952a0cb97
palibrix <theopaolo04@gmail.com>
2022-04-25 19:46:35 +0300

    Fully working logging in and logging out

--- MedSite/core/urls.py
+++ MedSite/core/urls.py
@@ -15,9 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.views.generic import TemplateView
 
 urlpatterns = [
-    path('base/', include('main.urls')),
+    path('', include('main.urls')),
     path('admin/', admin.site.urls),
     path('auth/', include('authentication.urls')),
+    path('', TemplateView.as_view(template_name='main/mainPage.html'), name='home'),
+    # path("accounts/", include("django.contrib.auth.urls")),
 ]
