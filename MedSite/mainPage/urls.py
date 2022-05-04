6082201569708483e5d8fb7e06c2bb4968e11b6a
skxlpv <olexandertym0000@gmail.com>
2022-05-04 15:23:10 +0300

    Settings page creation

--- MedSite/mainPage/urls.py
+++ MedSite/mainPage/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from mainPage import views
-
 urlpatterns = [
     path('', views.MainPage.as_view(), name='mainPage'),
+    path('settings', views.SettingsPage, name='settings'),
 ]
--- MedSite/mainPage/views.py
+++ MedSite/mainPage/views.py
@@ -1,5 +1,7 @@
 from django.contrib.auth.views import LoginView
 from django.shortcuts import render
+from authentication.models import User
+
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 
@@ -12,3 +14,5 @@ class MainPage(LoginView):
     # def get_success_url(self):
     #     return reverse_lazy('authentication')
 
+def SettingsPage(request):
+    return render (request, 'settingsPage/settingsPage.html')
