929c50beffac18700966992cbdeeaa629ab61bf7
skxlpv <olexandertym0000@gmail.com>
2022-05-11 17:17:56 +0300

    Uncomented database.

--- med_site/med_site/settings.py
+++ med_site/med_site/settings.py
@@ -87,16 +87,16 @@ STATICFILES_DIRS = [os.path.join(BASE_DIR, 'med_site/static')]
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': env.str('DATABASE_NAME'),
-#         'USER': env.str('DATABASE_USER'),
-#         'PASSWORD': env.str('DATABASE_PASS'),
-#         'HOST': env.str('DATABASE_HOST'),
-#         'PORT': env.str('DATABASE_PORT'),
-#     }
-# }
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': env.str('DATABASE_NAME'),
+        'USER': env.str('DATABASE_USER'),
+        'PASSWORD': env.str('DATABASE_PASS'),
+        'HOST': env.str('DATABASE_HOST'),
+        'PORT': env.str('DATABASE_PORT'),
+    }
+}
 
 
 # Password validation
